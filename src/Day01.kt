006687bde0d6b0e13c13f0d87102a2987d9a3d6b
Sam Castle <samcastle@google.com>
2022-12-02 13:02:37 -0800

    Day 1 basic solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        var max = 0
+        input.forEach {
+            if (it == "") {
+                max = max(sum, max)
+                sum = 0
+            }
+            else {
+                sum += it.toInt()
+            }
+        }
+        // one more time on the end
+        return max(sum, max)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var top3 = mutableListOf(0, 0, 0) // keep sorted with smallest item first.
+        var sum = 0
+        input.forEach {
+            if (it == "") {
+                if (sum > top3[0]) {
+                    top3[0] = sum
+                    top3 = top3.sorted().toMutableList()
+                }
+                sum = 0
+            }
+            else {
+                sum += it.toInt()
+            }
+        }
+        // one more time on the end.
+        if (sum > top3[0]) {
+            top3[0] = sum
+            top3 = top3.sorted().toMutableList()
+        }
+
+        return top3.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //println(part1(testInput))
+    check(part1(testInput) == 24000)
+    println(part2(testInput))
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("part1 = ${part1(input)}")
+    println("part2 = ${part2(input)}")
 }
